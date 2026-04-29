ab470580cb8a2a9ea625ea9c9bb7a74de2dc0200
an020202 <zfuxiang272@gmail.com>
2026-04-29 11:47:12 +0800

    week10

---
+++ week10/week10-2.cpp
@@ -0,0 +1,20 @@
+///week10-2.cpp 另一種排序法
+#include <stdio.h>
+int main()
+{
+    int a[10]={9,8,7,6,5,4,3,2,1,0};
+    for(int k=0;k<100;k++)printf("%d ",a[k]);
+    printf("\n");
+
+    for(int i=0;i<10;i++){
+        for(int j=i+1;j<10;i++){
+            if(a[i]>a[j]){
+               int temp=a[i] ;
+               a[i]=a[j];
+               a[j]=temp;
+            }
+        }
+    }
+    for(int k=0;k<10;k++)printf("%d ",a[k]);
+    printf("\n");
+}
